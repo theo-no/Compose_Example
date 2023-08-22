6f7de83f8616df78b3e904a2b201d34d96fd009a
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-08-22 11:08:02 +0900

    Column 예시 완료

--- Compose_Example/app/src/main/java/com/csh/compose_example/ColumnActivity.kt
+++ Compose_Example/app/src/main/java/com/csh/compose_example/ColumnActivity.kt
@@ -3,13 +3,18 @@ package com.csh.compose_example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.csh.compose_example.ui.theme.Compose_ExampleTheme
 
 class ColumnActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class ColumnActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ColumnEx()
                 }
             }
         }
@@ -30,17 +35,29 @@ class ColumnActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ColumnEx() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.size(200.dp)
+    ){
+        Text(
+            "첫 번째",
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Text(
+            "두 번째"
+        )
+        Text(
+            "세 번째"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     Compose_ExampleTheme {
-        Greeting("Android")
+        ColumnEx()
     }
 }

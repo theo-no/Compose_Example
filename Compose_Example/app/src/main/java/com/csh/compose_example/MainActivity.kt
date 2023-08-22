6ad124ebb0f99ea9df5a6642b70258edcd7eac43
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-08-22 10:02:30 +0900

    modifier / surface 예시 완료

--- Compose_Example/app/src/main/java/com/csh/compose_example/MainActivity.kt
+++ Compose_Example/app/src/main/java/com/csh/compose_example/MainActivity.kt
@@ -6,12 +6,17 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -42,36 +47,47 @@ class MainActivity : ComponentActivity() {
             Compose_ExampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surface
                 ) {
-                    CustomButton(onButtonClicked = {
-                        Log.d(TAG, "onClicked...")
-                        Toast.makeText(this, "onClick....", Toast.LENGTH_SHORT).show()
-                    }) //위임 받아서 여기 코드가 돌아감
+                    //위임 받아서 여기 코드가 돌아감
+                    CustomText(name = "android")
                 }
             }
         }
     }
 }
 
-
+ //////////////////////////Text //////////////////////////////////////////
 @Composable
 fun CustomText(name: String) {
-    Text(
-        color = Color.Red,
-        text = "Hello $name!\nHello $name!\nHello $name!",
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Bold,
-        fontFamily = FontFamily.Cursive,
-        letterSpacing = 2.sp,
-        maxLines = 2,
-        textDecoration = TextDecoration.Underline,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.width(300.dp)
-    )
+     Surface(
+         modifier = Modifier.padding(10.dp),
+         shadowElevation = 10.dp,
+//         tonalElevation = 10.dp,
+         border = BorderStroke(
+             width = 2.dp,
+             color = Color.Gray
+         ),
+         shape = CircleShape,
+         color = MaterialTheme.colorScheme.error
+     ) {
+         Text(
+//             color = Color.Black,
+             text = "Hello $name!",
+             fontSize = 30.sp,
+             fontWeight = FontWeight.Bold,
+//        fontFamily = FontFamily.Cursive,
+             letterSpacing = 2.sp,
+//        maxLines = 2,
+//             textDecoration = TextDecoration.Underline,
+             textAlign = TextAlign.Center,
+             modifier = Modifier.width(300.dp)
+         )
+     }
+
 }
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun CustomTextPreview() {
     Compose_ExampleTheme {
@@ -79,14 +95,25 @@ fun CustomTextPreview() {
     }
 }
 
+ /////////////////////////////Button /////////////////////////////////////
  @Composable
  fun CustomButton(onButtonClicked: () -> Unit){
+
     Button(
         onClick = onButtonClicked,
 //        enabled = false,
         border = BorderStroke(1.dp, Color.Black), //테두리
         shape = RoundedCornerShape(10.dp), //모양 설정(값 하나만 주면 모서리에 모두 10dp만큼 둥글게 설정)
         contentPadding = PaddingValues(10.dp), //padding 값 설정(값 하나만 주면 전체에 같은 값 패딩 적용)
+        modifier = Modifier
+            .wrapContentSize()
+//            .width(150.dp)
+//            .height(200.dp)
+            .padding(5.dp),
+        colors = ButtonDefaults.buttonColors( //버튼의 color는 colors 이용해서 바꿔라
+            containerColor = Color.Gray,
+            contentColor = Color.DarkGray
+        )
     ){
         Icon(
             imageVector = Icons.Filled.Send,
@@ -95,12 +122,16 @@ fun CustomTextPreview() {
         Spacer(
             modifier = Modifier.size(ButtonDefaults.IconSpacing) //제공하는 icon 사이 여백 값(dp로 임의로 줘도 됨)
         )
-        Text("Button")
+        Text(
+            "Button",
+//            modifier = Modifier.clickable {  },
+            modifier = Modifier.offset(x = 5.dp)
+        )
     }
  }
 
 
- @Preview(showBackground = true)
+// @Preview(showBackground = true)
  @Composable
  fun CustomButtonPreview() {
      Compose_ExampleTheme {
